c5192329bf25a34c0fee7553a958534e4b3ac742
tina1510 <141275553+tina1510@users.noreply.github.com>
2024-01-03 10:08:12 +0000

    Deploying to gh-pages from @ tina1510/goit-js-hw-11@029f88a4672c7afd8dcd5e1894d88db3c70805c7 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,20 +1,20 @@
-import{i as u,S as m}from"./assets/vendor-46aac873.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const f=document.querySelector("form"),h=document.querySelector(".gallery"),l=document.querySelector(".loader");function p(){l.classList.remove("hidden")}function a(){l.classList.add("hidden")}a();f.addEventListener("submit",s=>{s.preventDefault();const t=s.currentTarget.elements.query.value;t.length<2?alert("Your length is not enough. Min 3 letters"):v(t)});const g=(s="")=>{const t=new URLSearchParams({key:"41516813-c0516a6d5bb80b940f21213c5",q:s,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`https://pixabay.com/api/?${t}`).then(o=>{if(o.ok)return o.json();throw new Error("Error")})};function y(s){h.innerHTML=s.reduce((t,{webformatURL:o,largeImageURL:i,tags:e,likes:r,views:n,comments:c,downloads:d})=>t+`
+import{i as l,S as f}from"./assets/vendor-46aac873.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const h=document.querySelector("form"),p=document.querySelector(".gallery"),c=document.querySelector(".loader");function g(){c.classList.remove("hidden")}function a(){c.classList.add("hidden")}a();h.addEventListener("submit",s=>{s.preventDefault();const t=s.currentTarget.elements.query.value;t.length<=3?l.error({message:"Your length is not enough. Min 3 letters",position:"topRight"}):L(t)});const y=(s="")=>{const t=new URLSearchParams({key:"41516813-c0516a6d5bb80b940f21213c5",q:s,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`https://pixabay.com/api/?${t}`).then(o=>{if(o.ok)return o.json();throw new Error("Error")})};function v(s){const t=s.reduce((o,{webformatURL:n,largeImageURL:e,tags:r,likes:i,views:d,comments:u,downloads:m})=>o+`
             <div class="gallery-item">
-                <a href="${i}" data-lightbox="image">
-                    <img src="${o}" data-source="${i}" alt="${e}">
+                <a href="${e}" data-lightbox="image">
+                    <img src="${n}" data-source="${e}" alt="${r}">
                 </a>
                 <div class="text">
                 <div class="text-item"><p class="likes">Likes:</p>
-                <p class="likes"> ${r}</p></div>
+                <p class="likes"> ${i}</p></div>
                 
                <div  class="text-item"> <p class="views">Views: </p>
-                <p class="views">${n}</p></div>
+                <p class="views">${d}</p></div>
                <div  class="text-item"> <p class="comments">Comments:</p>
-                <p class="comments"> ${c}</p></div>
+                <p class="comments"> ${u}</p></div>
                <div  class="text-item"><p class="downloads">Downloads:</p>
-                <p class="downloads"> ${d}</p></div>
+                <p class="downloads"> ${m}</p></div>
                 
                 </div>
                 
-            </div>`,"")}function v(s){p(),g(s).then(t=>{a(),t.hits.length===0?u.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}):(y(t.hits),new m(".gallery a",{}).refresh())}).catch(t=>{a(),alert(t)})}
+            </div>`,"");p.insertAdjacentHTML("afterbegin",t)}function L(s){g(),y(s).then(t=>{a(),t.hits.length===0?l.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}):(v(t.hits),new f(".gallery a",{}).refresh())}).catch(t=>{a(),alert(t)})}
 //# sourceMappingURL=commonHelpers.js.map
